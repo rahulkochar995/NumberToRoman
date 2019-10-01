c437b4d839c143e898421d28c1f70a48bd323c35
rahulkochar995 <56028647+rahulkochar995@users.noreply.github.com>
2019-10-01 15:13:17 +0200

    Added documentations for HashMap

--- src/main/java/Main/main.java
+++ src/main/java/Main/main.java
@@ -4,11 +4,15 @@ import java.util.HashMap;
 import java.util.HashSet;
 
 public class main {
+    
+    //This is the main method.
     public static void main(String[] args) {
         System.out.println("Hello world");
 
+        //HashMap takes String as input and returns the corresponding number between 1-9.
         HashMap<String, Integer> dictionary = new HashMap<String, Integer>();
 
+        //Adding data to the HashMap
         dictionary.put("I", 1);
         dictionary.put("V", 5);
         dictionary.put("X", 10);
