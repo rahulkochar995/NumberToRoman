a7278ee97669d00ab29f0ec17be4346eeba448c8
rkochar <rkochar9@gmail.com>
2019-10-01 15:05:25 +0200

    Updated dictionary

--- src/main/java/Main/main.java
+++ src/main/java/Main/main.java
@@ -1,7 +1,16 @@
 package Main;
 
+import java.util.HashMap;
+import java.util.HashSet;
+
 public class main {
     public static void main(String[] args) {
         System.out.println("Hello world");
+
+        HashMap<String, Integer> dictionary = new HashMap<String, Integer>();
+
+        dictionary.put("I", 1);
+        dictionary.put("V", 5);
+        dictionary.put("X", 10);
     }
 }
