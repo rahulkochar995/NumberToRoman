5afaa0b4166c447bf56ecf4d825bcf0a3335201d
Rahul Kochar <r.kochar@student.tudelft.nl>
2019-10-01 12:35:56 +0200

    Added dictionary

--- src/main/java/Main/main.java
+++ src/main/java/Main/main.java
@@ -1,7 +1,14 @@
 package Main;
 
+import java.util.HashMap;
+
 public class main {
     public static void main(String[] args) {
         System.out.println("Hello world");
+
+        HashMap<String, Integer> dictionary = new HashMap<String, Integer>();
+
+        dictionary.put("I", 1);
+        dictionary.put("V", 5);
     }
 }
